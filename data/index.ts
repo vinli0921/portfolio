5801d7b5c8e80f7c967362103bf6031647be2c3a
Vincent Li <vincentli0921@gmail.com>
2024-05-31 23:06:52 -0700

    updated project cards and footer

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,6 @@
 export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
   
@@ -72,35 +71,35 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
+      title: "Neural Network for Handwriting Detection",
+      des: "Neural network project built for the UC Berkely CS 188 course to recognize handwritten numbers.",
+      img: "/nn.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      link: "https://github.com/vinny9541/CS-188",
     },
     {
       id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
+      title: "LeVox: AI Translation ",
+      des: "A speech translator that utilizes the open source WhisperAI Library, Google Cloud TTS Library, and DeepL API.",
+      img: "/levox.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
+      link: "https://github.com/vinny9541/LeAlgorithm",
     },
     {
       id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
+      title: "Recyclify: Making Recycling Simpler",
+      des: "A binary image classification program determining recyclability of personal waste using Keras from Tensorflow.",
+      img: "/recyclify.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
+      link: "https://github.com/vinny9541/BostonHacks",
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
+      title: "College Mart: Get More for Less",
+      des: "A discount service similiar to Honey but for restaurants targeted towards college students.",
+      img: "/dishcount.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "https://github.com/SamuelLo1/college-mart",
     },
   ];
   
@@ -173,29 +172,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Frontend Engineer",
+      desc: "Assisted in the development of web-based platforms using React.js and Next.js, enhancing interactivity.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "AI Developer",
+      desc: "Designed and developed mutiple AI projects including image classification and NLP.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Course Assistant",
+      desc: "Helped TAs facilitate labs, hosted office hours, graded assignments, and answered piazza inquiries.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Hackathon Lead Backend Developer",
+      desc: "Oversaw idea consolidation of my hackathon projects and coordinated repository contributions.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
@@ -205,13 +204,16 @@ export const navItems = [
     {
       id: "1",
       img: "/git.svg",
+      link: "https://github.com/vinny9541/",
     },
     {
       id: "2",
       img: "/twit.svg",
+      link: " ",
     },
     {
       id: "3",
       img: "/link.svg",
+      link:"https://www.linkedin.com/in/vinli0921/",
     },
   ];
